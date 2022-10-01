c0ac4181ca8e71950656034af07ba29bf323cde4
Chris Blanquera <chris@incept.asia>
2022-10-01 23:33:43 +0800

    removing migrations

--- prisma/migrations/20221001061134_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Consumer` MODIFY `walletAddress` VARCHAR(255) NOT NULL;
--- prisma/migrations/20221001144346_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `balance` on the `Consumer` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Consumer` DROP COLUMN `balance`,
-    ADD COLUMN `consumed` VARCHAR(191) NOT NULL DEFAULT '0';
